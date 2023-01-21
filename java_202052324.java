3ac53400ec70a9453fd732d55e8b33a71ae6c80a
lokesh <lokeshchoudharykr@gmail.com>
2023-01-21 15:35:32 +0530

    new file

--- java_202052324.java
+++ java_202052324.java
@@ -1,5 +1,6 @@
 class java_202052324 {
     public static void main(String[] args) {
         System.out.println("hello this is the second project on jenkins");
+        System.out.println("this line is a change ");
     }
 }

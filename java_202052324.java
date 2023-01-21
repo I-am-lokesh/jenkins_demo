b6c56e9c1fdc39fd504d15a4582dfaa2e158b096
lokesh <lokeshchoudharykr@gmail.com>
2023-01-22 01:00:58 +0530

    New line added

--- java_202052324.java
+++ java_202052324.java
@@ -1,6 +1,8 @@
 class java_202052324 {
     public static void main(String[] args) {
-        System.out.println("hello this is the second project on jenkins");
+        System.out.println("hello this is the fifth project on jenkins");
         System.out.println("this line is a change ");
+        System.out.println(":This is a new line");
+
     }
 }
